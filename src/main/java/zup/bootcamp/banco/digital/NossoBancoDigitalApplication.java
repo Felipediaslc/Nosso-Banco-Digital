14435f06809dc0de4881d92485db12479eddcd29
williamisfranciscodasilva <williamisepaula@gmail.com>
2020-10-15 06:02:53 -0300

    Iniciando o projeto Nosso Banco Digital

---
+++ src/main/java/zup/bootcamp/banco/digital/NossoBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package zup.bootcamp.banco.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zup/bootcamp/banco/digital/NossoBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package zup.bootcamp.banco.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
